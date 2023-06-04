f62366d4870ef30e0c629dec197bf0129fa2b1fb
ParamSurywashi <parmanandchared1997@gmail.com>
2023-06-04 17:47:09 +0530

    update vite dist

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ const root = resolve(__dirname, "src");
 module.exports = {
     root: root,
     build: {
+      outDir: "../dist",
       rollupOptions: {
         input: {
             main: resolve(root, 'index.html')
